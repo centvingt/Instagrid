96402dfac0c79592d036b44b1062684ee44674f1
Vincent Caronnet <120@120.design>
2021-04-05 22:48:52 +0200

    Remplacement du texte du LaunchScreen par une image Le bouton touché a son isSelected qui passe à true et les autres boutons ont leur isSelected qui passe à false Implémentation de la UICollectionVIewDataSource

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var instructions: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button0DidTouched(_ sender: Any) {
+        buttons.forEach { (button) in
+            button.isSelected = false
+        }
+        buttons[0].isSelected = true
+    }
+    @IBAction func button1DidTouched(_ sender: Any) {
+        buttons.forEach { (button) in
+            button.isSelected = false
+        }
+        buttons[1].isSelected = true
+    }
+    @IBAction func button2DidTouched(_ sender: Any) {
+        buttons.forEach { (button) in
+            button.isSelected = false
+        }
+        buttons[2].isSelected = true
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "cell",
+            for: indexPath
+        )
+        return cell
+    }
+    
+    
+}
